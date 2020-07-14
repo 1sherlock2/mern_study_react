357a956d6052e31db1d6ebe48f67b1b14773f381
sherlock <alim.gazdiev@gmail.com>
2020-07-14 13:39:04 +0500

    Updates

---
+++ precache-manifest.98deaedc8ff3e579e3dcc2e7336d8304.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb369e02ef37d74d77a240c21f7e5746",
+    "url": "/mern_study_react/index.html"
+  },
+  {
+    "revision": "637320e29c02950e8f9f",
+    "url": "/mern_study_react/static/css/3.4d2b15a9.chunk.css"
+  },
+  {
+    "revision": "3529f83721cef51d594f",
+    "url": "/mern_study_react/static/css/4.b0288567.chunk.css"
+  },
+  {
+    "revision": "5b4f2fca67b778c44672",
+    "url": "/mern_study_react/static/css/5.e1280ca6.chunk.css"
+  },
+  {
+    "revision": "eee660a22c6ff67d5d0a",
+    "url": "/mern_study_react/static/css/6.27a1b572.chunk.css"
+  },
+  {
+    "revision": "266afcecb534f800fbcc",
+    "url": "/mern_study_react/static/css/main.224d343d.chunk.css"
+  },
+  {
+    "revision": "4612f3630e384b7ae4b7",
+    "url": "/mern_study_react/static/js/2.de85de16.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mern_study_react/static/js/2.de85de16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637320e29c02950e8f9f",
+    "url": "/mern_study_react/static/js/3.e0fbea94.chunk.js"
+  },
+  {
+    "revision": "3529f83721cef51d594f",
+    "url": "/mern_study_react/static/js/4.9f589222.chunk.js"
+  },
+  {
+    "revision": "5b4f2fca67b778c44672",
+    "url": "/mern_study_react/static/js/5.cb8b115e.chunk.js"
+  },
+  {
+    "revision": "eee660a22c6ff67d5d0a",
+    "url": "/mern_study_react/static/js/6.da77bb48.chunk.js"
+  },
+  {
+    "revision": "266afcecb534f800fbcc",
+    "url": "/mern_study_react/static/js/main.14d27aef.chunk.js"
+  },
+  {
+    "revision": "fb40a994f67019e8eacb",
+    "url": "/mern_study_react/static/js/runtime-main.3f331e94.js"
+  }
+]);
